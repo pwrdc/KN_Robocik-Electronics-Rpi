c83c06efc04148f4e4ee06659102e104b3a15172
Igor0663 <igorzielinski3c@wp.pl>
2019-07-05 15:04:18 +0200

    Added bluerobotics sonar library

---
+++ ping-python/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(name='bluerobotics-ping',
+      version='0.0.7',
+      description='A python module for the Blue Robotics Ping1D echosounder',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='Blue Robotics',
+      author_email='support@bluerobotics.com',
+      url='https://www.bluerobotics.com',
+      packages=find_packages(), install_requires=['pyserial', 'future'],
+      classifiers=[
+          "Programming Language :: Python",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
+      scripts=[
+          "examples/simplePingExample.py",
+          "tools/pingproxy.py",
+          "tools/ping1d-simulation.py"]
+      )
