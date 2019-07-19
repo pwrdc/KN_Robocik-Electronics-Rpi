1dc71ad828a6aad907f5c806b33f53ccefca3303
DominikWasiolka <dominowasy@gmail.com>
2019-07-19 14:23:30 +0200

    depth.py reconstruction

--- depth.py
+++ depth.py
@@ -1,28 +1,34 @@
 import ms5837
 import time
 
-from ports import DEPTH_DRIVER_PORT, DEPTH_CLIENT_PORT
-from rov_comm import Client, ZMQ_Server
 from logpy.LogPy import Logger
 
-SCALETOCM = 1.019
-
+from ports import DEPTH_DRIVER_PORT, DEPTH_CLIENT_PORT
+from definitions import DEFLOG
+from rov_comm import Client
 
 class DepthSensor:
     def __init__(self):
-        self.sensor = ms5837.MS5837_30BA() # Default I2C bus is 1 (Raspberry Pi 3)        
+        self.sensor = ms5837.MS5837_02BA() # Default I2C bus is 1 (Raspberry Pi 3)        
         self.client = Client(DEPTH_DRIVER_PORT)
-        #self.logger = Logger(filename='depth')
+
+        self.logger = DEFLOG.DEPTH_LOCAL_LOG
+        if self.logger:
+            self.logger = Logger(filename='depth_sensor', directory=DEFLOG.LOG_DIRECTORY)
 
     def run(self):
         # We must initialize the sensor before reading it
         if not self.sensor.init():
-            print("Sensor could not be initialized")
+            error = "Sensor could not be initialized"
+            self.log(error, 'error')
+            print(error)
             exit(1)
 
         # We have to read values from sensor to update pressure and temperature
         if not self.sensor.read():
-            print("Sensor read failed!")
+            error = "Sensor read failed!"
+            self.log(error, 'error')
+            print(error)
             exit(1)
 
         loop_condition = True
@@ -30,22 +36,18 @@ class DepthSensor:
             if self.sensor.read():
                 self.client.send_data(self.sensor.depth())
 
-                #print(self.sensor.pressure())
                 msg = str(self.sensor.depth())
-                #print(msg)
-                #self.logger.log(msg)
+                self.logger.log(msg)
             else:
                 loop_condition = False
+
     def __del__(self):
-        pass
-        #self.logger.exit()
+        self.logger.exit()
+
+    def log(self, msg, logtype='info'):
+        if self.logger:
+            self.logger.log(msg, logtype=logtype)
 
 if __name__ == '__main__':
-    
-    #server = ZMQ_Server(DEPTH_DRIVER_PORT, DEPTH_CLIENT_PORT)
-    #server.run()
-    
     depth_sensor = DepthSensor()
     depth_sensor.run()
-
-    # in case of sensor error
