fea40bbfce2266b6e099c8567ede45817527f8fb
DominikWasiolka <dominowasy@gmail.com>
2019-07-19 16:30:11 +0200

    add definitions.py

---
+++ definitions.py
@@ -0,0 +1,21 @@
+MODE = 'ROV3' # 'ROV3' or 'ROV4'
+
+
+class DEFLOG:
+    LOG_DIRECTORY = "logs/"
+
+    DEPTH_LOCAL_LOG = True
+
+    AHRS_LOCAL_LOG = True
+
+    HYDROPHONES_LOCAL_LOG = False
+
+    DISTANCE_LOCAL_LOG = True
+
+    MOVEMENTS_LOCAL_LOG = True
+    MANIPULATOR_LOCAL_LOG = False
+    TORPEDOES_LOCAL_LOG = False
+
+class USB:
+    # Default mount point of usb sensors
+    SONAR = "/dev/ttyUSB1"
