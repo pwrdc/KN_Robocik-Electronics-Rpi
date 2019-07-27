5cb2d5b1866f07334c1df5ea26a82e1c5941b1d8
Karol <karol.kosciany@gmail.com>
2019-07-27 17:14:14 +0200

    Torpedoes updated
    Second server, ports added, torpedoes.py changed. Now it should work correctly

---
+++ torpedoes_el.py
@@ -0,0 +1,32 @@
+from ports import TORPEDO_FIRE_DRIVER_PORT, TORPEDO_READY_CLIENT_PORT
+from rov_comm import Client
+import time
+
+class Torpedoes:
+    is_ready = False
+    def __init__(self):
+        self.client_fire = Client(TORPEDO_FIRE_DRIVER_PORT)
+        self.client_ready = Client(TORPEDO_READY_CLIENT_PORT)
+
+    def fire(self):
+        command = self.client_fire.get_data()
+        print(command)
+        if command == "FIRE" and self.is_ready:
+            print("I na koniec z obrotówy jeb") #tutaj szczelomy
+            self.client_ready.send_data('WAIT')
+            self.reload_torpedo()
+
+    def reload_torpedo(self):
+        reload_time = 2   # czas przeładowywania, do ustawienia
+        """
+        TUTAJ PRZEŁADOWANIE
+        """
+        time.sleep(reload_time)  # jak umiecie jakieś lepsze czekanie, to zmieńcie
+        self.client_ready.send_data('READY')
+
+if __name__ == "__main__":
+    torpedoes = Torpedoes()
+
+    while True:
+        torpedoes.fire()
+        time.sleep(0.02)
